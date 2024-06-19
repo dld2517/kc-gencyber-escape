9aa5d72769fa2d7f603315fd1191268b342ecdae
Danny Darden <10632644+dld2517@users.noreply.github.com>
2024-06-19 03:31:11 -0500

    Briefcase Escape file
    Updated as I was trying to get it to work on Repl.it deploy

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
-# import replit
-# replit.clear()
 import os
-import pygame
 
-loop=True
+loop=1
 os.system('clear')
 
-
-pygame.init()
-x=400
-y=400
-scrn = pygame.display.set_mode((x,y))
-pygame.display.set_caption('image')
-imp = pygame.image.load("./blog.png").convert()
-scrn.blit(imp, (0, 0))
-pygame.display.flip()
-status = True
 
 def briefcase():
   os.system('clear') 
   i = 0
   while (i < 3):
-      print('What is the combination (6 digits)?')
+      print('Briefcase\nWhat is the combination (6 digits)?')
       combination = input('(To escape script, type "e")\n')
       if (combination == "e"):
-            return;
+            return 0
       if (combination == "013088"):
-            print("Correct! Take card #7\n")
-            return;
+            message="Correct! One step closer!\n"
+            card=7
+            menu(message,card)
       elif (i == 0): # First incorrect guess
             print("Incorrect, try again.\n")
             i = i+1
@@ -37,17 +25,21 @@ def briefcase():
       else: # Three strikes you're out!
             print("Incorrect. Take card #15\n")
             i = i+1
+            return 0
+  
+
 def laptop():
     os.system('clear')
     i = 0
     while (i < 3):
-        print('What is the password?')
+        print('Laptop\nWhat is the password?')
         combination = input('(To escape script, type "e")\n')
         if (combination == "e"):
-            return;
+            return 0
         if (combination == "password"):
-            print("Correct! Take card #8\n")
-            return;
+            message="Correct! One step closer!\n"
+            card=8
+            menu(message,card)
         elif (i == 0): # First incorrect guess
             print("Incorrect, try a very obvious and insecure password.\n")
             i = i+1
@@ -57,17 +49,20 @@ def laptop():
         else: # Three strikes you're out!
             print("Incorrect. Take card #29\n")
             i = i+1
+            return 0
+            
 def oranges():
     os.system('clear')
     i = 0
     while (i < 3):
-        print('When will Suzanne attack? (Month day)')
+        print('Oranges\nWhen will Suzanne attack? (Month day)')
         combination = input('(To escape script, type "e")\n')
         if (combination == "e"):
-            return;
+            return 0
         if (combination == "June 7" or combination == "June 07"):
-            print("Correct! Take card #6\n")
-            return;
+            message="Correct! One step closer!\n"
+            card=6
+            menu(card,message)
         elif (i == 0): # First incorrect guess
             print("Incorrect, try again.\n")
             i = i+1
@@ -77,17 +72,20 @@ def oranges():
         else: # Three strikes you're out!
             print("Incorrect. Take card #2\n")
             i = i+1
+            return 0
+            
 def phone():
     os.system('clear')
     i = 0
     while (i < 3):
-        print('What is the password?')
+        print('Phone\nWhat is the password?')
         combination = input('(To escape script, type "e")\n')
         if (combination == "e"):
-            return;
+            return 0
         if (combination == "enigmamachine"):
-            print("Correct! Take card #14\n")
-            return;
+            message="Correct! One step closer!\n"
+            card=14
+            menu(message,card)
         elif (i == 0): # First incorrect guess
             print("Incorrect, try again.\n")
             i = i+1
@@ -95,19 +93,22 @@ def phone():
             print("Incorrect, try again (lowercase and no space).\n")
             i = i+1
         else: # Three strikes you're out!
-            print("Incorrect. Take card #30\n")
+            message="Incorrect. Take card #30\n"
             i = i+1
+            return 0
+            
 def target():
     os.system('clear')
     i = 0
     while (i < 3):
-        print('Who will Suzanne attack? (FIRST LAST)\n')
+        print('Target\nWho will Suzanne attack? (FIRST LAST)\n')
         combination = input('(To escape script, type "e")\n')
         if (combination == "e"):
-            return;
+            return 0
         if (combination == "ZOE WALKER"):
-            print("Correct! Take card #23\n")
-            return;
+            message="Correct! Almost there!\n"
+            card=23
+            menu(message,card)
         elif (i == 0): # First incorrect guess
             print("Incorrect, try again.\n")
             i = i+1
@@ -115,18 +116,20 @@ def target():
             print("Incorrect, try first and last name with space and all caps.\n")
             i = i+1
         else: # Three strikes you're out!
-            print("Incorrect. Take card #5\n")
+            message="Incorrect. Take card #5\n"
             i = i+1
+            return 0
+            
 def train():
     os.system('clear')
     i = 0
     while (i < 3):
-        print('Where will Suzanne attack? (PLACENAME)\n')
+        print('Train\nWhere will Suzanne attack? (PLACENAME)\n')
         combination = input('(To escape script, type "e")\n')
         if (combination == "e"):
-            return;
+            return 0
         if (combination == "DISNEYLANDANAHEIM"):
-            message="Correct! "
+            message="Correct! You have SAVED THE DAY!"
             card=3
             menu(message,card)
         elif (i == 0): # First incorrect guess
@@ -136,34 +139,33 @@ def train():
             print("Incorrect, try all caps with no space between words.\n")
             i = i+1
         else: # Three strikes you're out!
-            message="Inorrect! "
-            card=26
-            menu(message,card)
-            i = i+1
+            message="Incorrect! Take card #26\n"
+            i=i+1
+            return 0
+            
 def wallet():
     os.system('clear')
     i = 0
     while (i < 3):
-        print('What is the combination (NNN)?')
+        print('Wallet\nWhat is the combination (NNN)?')
         combination = input('(To escape script, type "e")\n')
         if (combination == "e"):
-            return;
+            return 0
         if (combination == "798"):
-            message="Correct! "
+            message='Correct! '
             card=9
             menu(message,card)
         elif (i == 0): # First incorrect guess
-            print("Incorrect, try again.\n")
+            print('Incorrect, try again.\n')
             i = i+1
         elif (i == 1): # Second incorrect guess
-            print("Incorrect, look carefully at the watch.\n")
+            print('Incorrect, look carefully at the watch.\n')
             i = i+1
         else: # Three strikes you're out!
-            message="Inorrect! "
-            card=33
-            menu(message,card)
-            i = i+1
-
+            message='Incorrect! Take card #6\n'
+            i=i+1
+            return 0
+            
 def menu(message,card):
     print ('\n\n\nWill you escape?\n\n')
     print (message,'See card',card,'for instructions!')
@@ -192,16 +194,14 @@ def menu(message,card):
     elif (choice == '7'):
       wallet()
     elif (choice == '8'):
-      loop=False
-      return loop
+      return 0
     else:
       print ('Wrong')
 
 while loop:
   print ('\n\n\nWill you escape?\n\n')
-  message="Use your crypto seneses!"
+  message='Use your crypto seneses!'
   card=0
   loop=menu(message,card)
 
-pygame.quit()
-print("Thanks for playing!")
+print('Thanks for playing!')
